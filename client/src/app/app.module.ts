158d9bdc804ea6f82ce91c771435f39b4cd13c1e
Arbuz <alex_sisg@vk.com>
2024-11-11 21:50:41 +0400

    добавил модальное окно авторизации

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,6 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FormsModule } from '@angular/forms';  
 
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -11,11 +16,12 @@ import { ProductPageComponent } from './pages/product-page/product-page.componen
 import { FilterPanelComponent } from './components/filter-panel/filter-panel.component';
 import { FilterItemComponent } from './components/filter-item/filter-item.component';
 import { SideFilterPanelComponent } from './components/side-filter-panel/side-filter-panel.component';
-
 import { ListProductCardCatalogComponent } from './components/list-product-card-catalog/list-product-card-catalog.component';
 import { ProductCardCatalogComponent } from './components/product-card-catalog/product-card-catalog.component';
 import { FilterAsidePanelComponent } from './components/filter-aside-panel/filter-aside-panel.component';
 import { FilterPipePipe } from './pipes/filter-pipe.pipe';
+import { ModalWindowSignInComponent } from './components/modal-window-sign-in/modal-window-sign-in.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 @NgModule({
   declarations: [
@@ -30,9 +36,12 @@ import { FilterPipePipe } from './pipes/filter-pipe.pipe';
     ProductCardCatalogComponent,
     FilterAsidePanelComponent,
     FilterPipePipe,
+    ModalWindowSignInComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule, FormsModule],
-  providers: [],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule, FormsModule, MatDialogModule, MatFormFieldModule, MatInputModule, MatButtonModule],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- client/src/app/components/navbar/navbar.component.ts
+++ client/src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
-
+import { MatDialog } from '@angular/material/dialog';
+import { ModalWindowSignInComponent } from '../modal-window-sign-in/modal-window-sign-in.component';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  constructor(private dialog: MatDialog) {}
 
+  openSignInModal(): void {
+    const dialogRef = this.dialog.open(ModalWindowSignInComponent, {
+      width: '400px',
+      disableClose: true,
+    });
+    dialogRef.backdropClick().subscribe(() => {
+      dialogRef.close(); // закрывает при клике на задний фон
+    });
+  }
 }
