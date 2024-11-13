9cb72f17b0bd9b224439125b7235c04133423450
Arbuz <alex_sisg@vk.com>
2024-11-13 20:19:18 +0400

    добавил модальное окно регистрации и сервис для работы с модальными окнами

--- client/src/app/components/modal-window-sign-in/modal-window-sign-in.component.ts
+++ client/src/app/components/modal-window-sign-in/modal-window-sign-in.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
-
-
+// import { MatDialog, MatDialogClose, MatDialogRef } from '@angular/material/dialog';
+// import { ModalWindowSignUpComponent } from '../modal-window-sign-up/modal-window-sign-up.component';
+// import { NavbarComponent } from '../navbar/navbar.component';
+import { ModalService } from '../../services/modal-service/modal.service';
 @Component({
   selector: 'app-modal-window-sign-in',
   templateUrl: './modal-window-sign-in.component.html',
   styleUrls: ['./modal-window-sign-in.component.scss']
 })
 export class ModalWindowSignInComponent {
+  constructor(private modalService: ModalService) {}
 
+  openSignUpModal(): void {
+    this.modalService.closeSignInModal()
+    this.modalService.openSignUpModal();
+  }
 }
