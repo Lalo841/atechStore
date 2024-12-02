208cb2fa6298a6b1cc5f498750685508de15f6ba
Arbuz <alex_sisg@vk.com>
2024-12-03 01:48:33 +0400

    валидация для модальных окон авторизации и регистрации

--- client/src/app/components/modal-window-sign-in/modal-window-sign-in.component.ts
+++ client/src/app/components/modal-window-sign-in/modal-window-sign-in.component.ts
@@ -1,18 +1,89 @@
-import { Component } from '@angular/core';
-// import { MatDialog, MatDialogClose, MatDialogRef } from '@angular/material/dialog';
-// import { ModalWindowSignUpComponent } from '../modal-window-sign-up/modal-window-sign-up.component';
-// import { NavbarComponent } from '../navbar/navbar.component';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ModalService } from '../../services/modal-service/modal.service';
+
 @Component({
   selector: 'app-modal-window-sign-in',
   templateUrl: './modal-window-sign-in.component.html',
-  styleUrls: ['./modal-window-sign-in.component.scss']
+  styleUrls: ['./modal-window-sign-in.component.scss'],
 })
-export class ModalWindowSignInComponent {
-  constructor(private modalService: ModalService) {}
+export class ModalWindowSignInComponent implements OnInit {
+  signInForm!: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private modalService: ModalService
+  ) {}
+
+  ngOnInit(): void {
+    this.signInForm = this.fb.group({
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.email,
+          Validators.minLength(10),
+          Validators.maxLength(20),
+        ],
+      ],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(16),
+        ],
+      ],
+    });
+  }
+
+  get email() {
+    return this.signInForm.get('email');
+  }
+
+  get password() {
+    return this.signInForm.get('password');
+  }
+  
+  getEmailErrorMessage(): string {
+    console.log('Проверка ошибки email', this.email?.errors);
+    if (this.email?.hasError('required')) {
+      return 'Email обязателен.';
+    }
+    if (this.email?.hasError('email')) {
+      return 'Пожалуйста, введите корректный email-адрес.';
+    }
+    if (this.email?.hasError('minlength')) {
+      return 'Длина email должна быть не менее 10 символов.';
+    }
+    if (this.email?.hasError('maxlength')) {
+      return 'Длина email должна быть не более 20 символов.';
+    }
+    return '';
+  }
+
+  getPasswordErrorMessage(): string {
+    if (this.password?.hasError('required')) {
+      return 'Пароль обязателен.';
+    }
+    if (this.password?.hasError('minlength')) {
+      return 'Длина пароля должна быть не менее 8 символов.';
+    }
+    if (this.password?.hasError('maxlength')) {
+      return 'Длина пароля должна быть не более 16 символов.';
+    }
+    return '';
+  }
+
+  onSubmit(): void {
+    if (this.signInForm.valid) {
+      alert('Форма успешно отправлена!');
+      // Здесь можно добавить логику отправки данных на сервер
+    }
+  }
 
   openSignUpModal(): void {
-    this.modalService.closeSignInModal()
+    this.modalService.closeSignInModal();
     this.modalService.openSignUpModal();
   }
 }
--- client/src/app/components/modal-window-sign-up/modal-window-sign-up.component.ts
+++ client/src/app/components/modal-window-sign-up/modal-window-sign-up.component.ts
@@ -1,10 +1,84 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ModalService } from '../../services/modal-service/modal.service';
 
 @Component({
   selector: 'app-modal-window-sign-up',
   templateUrl: './modal-window-sign-up.component.html',
-  styleUrls: ['./modal-window-sign-up.component.scss']
+  styleUrls: ['./modal-window-sign-up.component.scss'],
 })
-export class ModalWindowSignUpComponent {
+export class ModalWindowSignUpComponent implements OnInit {
+  signUpForm!: FormGroup;
 
+  constructor(
+    private fb: FormBuilder,
+    private modalService: ModalService
+  ) {}
+
+  ngOnInit(): void {
+    this.signUpForm = this.fb.group({
+      email: [
+        '', // Начальное значение
+        [
+          Validators.required,
+          Validators.email,
+          Validators.minLength(10),
+          Validators.maxLength(20),
+        ],
+      ],
+      password: [
+        '', // Начальное значение
+        [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(16),
+        ],
+      ],
+    });
+  }
+
+  get email() {
+    return this.signUpForm.get('email');
+  }
+
+  get password() {
+    return this.signUpForm.get('password');
+  }
+
+  getEmailErrorMessage(): string {
+    if (this.email?.hasError('required')) {
+      return 'Email обязателен.';
+    }
+    if (this.email?.hasError('email')) {
+      return 'Пожалуйста, введите корректный email-адрес.';
+    }
+    if (this.email?.hasError('minlength')) {
+      return 'Длина email должна быть не менее 10 символов.';
+    }
+    if (this.email?.hasError('maxlength')) {
+      return 'Длина email должна быть не более 20 символов.';
+    }
+    return '';
+  }
+
+  getPasswordErrorMessage(): string {
+    if (this.password?.hasError('required')) {
+      return 'Пароль обязателен.';
+    }
+    if (this.password?.hasError('minlength')) {
+      return 'Длина пароля должна быть не менее 8 символов.';
+    }
+    if (this.password?.hasError('maxlength')) {
+      return 'Длина пароля должна быть не более 16 символов.';
+    }
+    return '';
+  }
+
+  onSubmit(): void {
+    if (this.signUpForm.valid) {
+      alert('Регистрация успешна!');
+      console.log(this.signUpForm.value); // Логика отправки данных на сервер
+      this.modalService.closeSignUpModal();
+    }
+  }
 }
