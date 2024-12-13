67db46db9872f0ab1e28efcf2ccf8ab201a43544
Atech <A.Klimov03@mail.ru>
2024-12-13 11:23:04 +0400

    О системе, о разрабах

--- client/src/app/pages/devs-info-page/devs-info-page.component.ts
+++ client/src/app/pages/devs-info-page/devs-info-page.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-devs-info-page',
   templateUrl: './devs-info-page.component.html',
-  styleUrl: './devs-info-page.component.scss'
+  styleUrls: ['./devs-info-page.component.scss']
 })
-export class DevsInfoPageComponent {
+export class DevsInfoPageComponent implements OnInit {
 
+  images: string[] = [
+    'alex.jpg',
+    'egor.jpg',
+    'arb.jpg' 
+  ];
+
+  currentIndex: number = 0;
+  totalImages: number = this.images.length;
+
+  ngOnInit(): void {
+    setInterval(() => {
+      this.nextSlide();
+    }, 3000); // Сменить изображение каждые 3 секунды
+  }
+
+  nextSlide(): void {
+    this.currentIndex = (this.currentIndex + 1) % this.totalImages;
+  }
 }
